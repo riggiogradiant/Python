72f3478477646a767940da8914caeb3c09041502
riggiogradiant <criggio@gradiant.org>
2023-06-20 14:59:40 +0200

    update

--- try1.py
+++ try1.py
@@ -1,2 +1,10 @@
-len('Hello World')
-print('Hello World')
+Nombre = input('Inserta tu nombre completo: ')
+Edad = input('Inserta tu edad: ')
+
+print('Tu nombre es:', Nombre)
+print(type(Nombre))
+print('Tu edad es:', Edad)
+print(type(Edad))
+
+Edad= int(Edad)
+print('El nuevo tipo es: ', type(Edad))
